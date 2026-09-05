e05dece0fbece7db58d17ec2694d3dc68dd0e2f5
williamharris8 <williamharris8@student.bpcc.edu>
2026-09-05 10:30:19 -0500

    PHASE 3 COMPLETE

--- Journal.md
+++ Journal.md
@@ -1,3 +1,5 @@
 PHASE 1 - Similar to using MS Word. If we are trying to backtrack with an undo button, we want the last input at the top of the stack. FIFO would be more useful in the instance of a to do list.
 
 PHASE 2 - Handling this using return will exit the method early before reaching the pop line. Without it will throw EmptyStackException and crash.
+
+PHASE 3 - redoStack is holding what is undid if it's wanted back. If you undo something and then type something new, what is in the redo stack no longer belongs anymore. If that wasnt cleared pressing redo would swap the text you just typed for the old version.
--- src/EditorHistory.java
+++ src/EditorHistory.java
@@ -2,16 +2,25 @@ import java.util.Stack;
 
 public class EditorHistory {
     private Stack<String> undoStack = new Stack<>();
+    private Stack<String> redoStack = new Stack<>();
     private String currentState = "";
 
     public void makeChange(String newState) {
         undoStack.push(currentState);
         currentState = newState;
+        redoStack.clear();
 
     }
 
     public void undo() {
         if (undoStack.isEmpty()) return;
+        redoStack.push(currentState);
         currentState = undoStack.pop();
+    }
+
+    public void redo() {
+        if (redoStack.isEmpty()) return;
+        undoStack.push(currentState);
+        currentState = redoStack.pop();
     }
 }
